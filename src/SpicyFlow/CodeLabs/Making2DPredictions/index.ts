b76ccc1a0c80aaff98f27db3ae9749bdaa3cb55e
SpycyMike <75376905+SpycyMike@users.noreply.github.com>
2021-11-23 19:07:15 -0500

    Wait no I messed up the config. Fixed!

--- src/SpicyFlow/CodeLabs/Making2DPredictions/index.ts
+++ src/SpicyFlow/CodeLabs/Making2DPredictions/index.ts
@@ -1,7 +1,15 @@
 import { GetCarData } from "./CarService"
+import tf from '@tensorflow/tfjs-node'
 
 export const Make2DPrediction = async () => {
 
     const carData = await GetCarData()
     console.table(carData)
+}
+
+const CreateModel = () =>{
+        const model = tf.sequential();
+        model.add(tf.layers.dense({inputShape: [1], units: 1}))
+        model.add(tf.layers.dense({units: 1}))
+        return model;
 }
